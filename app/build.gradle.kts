6ddc853e62c0d6fe76de98ca32084be8eb47c503
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2025-07-21 23:47:16 +0530

    Bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         targetSdk = 28
 
         versionCode = 2
-        versionName = "0.2.18"
+        versionName = "0.2.19"
 
         buildConfigField("boolean", "GOLD", "false")
         buildConfigField("String", "POSTHOG_API_KEY", "\"$posthogApiKey\"")
